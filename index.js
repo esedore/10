272de543817f6ea478cc9e19f88020667d60e12c
DPI910Group5 <dpi910_group5@protonmail.com>
2022-04-11 23:39:08 -0400

    books

--- index.js
+++ index.js
@@ -3,48 +3,40 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const routes = require('./controllers/routes.js');
 const express = require('express');
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const document = (new JSDOM('')).window;
+global.document = document;
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://esedore:88eric88@cluster0.vbnij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+var mongodb = "mongodb+srv://esedore:88eric88@cluster0.vbnij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(mongodb, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const db = client.db("SRT621");
   let collection = db.collection("books");
   let b1Query = {book:'Neuromancer'}
   let b2Query = {book:'The Way of Kings'}
   let b3Query = {book:'The Sandman: Preludes & Nocturnes'}
-  collection.findOne(b1Query).then(doc => {
-
-    console.log(doc);
-
+  collection.findOne(b1Query).then(book1 => {
+    console.log(book1);
 }).catch((err) => {
-
     console.log(err);
 })
-
-collection.findOne(b2Query).then(doc => {
-
-  console.log(doc);
-
+collection.findOne(b2Query).then(book2 => {
+  console.log(book2);
 }).catch((err) => {
-
   console.log(err);
 })
-
-collection.findOne(b3Query).then(doc => {
-
-  console.log(doc);
-
+collection.findOne(b3Query).then(book3 => {
+  console.log(book3);
 }).catch((err) => {
-
   console.log(err);
 }).finally(() => {
-
   client.close();
 });
-
 });
 
 app.set('views', path.join(__dirname, 'html'));
@@ -57,7 +49,7 @@ app.set('view engine', 'ejs');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('');
+  res.send('');
 });
 
 app.use('/',routes);
